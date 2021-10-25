7f7d38420bde591ae283584f53ec0cdd97c8ec65
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-25 17:23:48 -0500

    Update App.js
    Adding BrowserRouter, Route, Switch and Redirect

--- gallery-app/src/App.js
+++ gallery-app/src/App.js
@@ -1,9 +1,29 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Config from './Components/Config';
+import apiKey from './Components/Config';
 
-function App() {
+class App extends Component {
+  
+  render() {
+    return(
+      <BrowserRouter>
+        <div className="container">
+          <SearchForm onSearch={} />
+          <Header />
 
+          <Switch>
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
