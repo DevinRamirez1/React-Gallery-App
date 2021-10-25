24e9f0550c7997b9e9ab510eb86efefe28a6cd60
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-25 17:42:02 -0500

    Update Header.js
    Typing out the skeletal code for Header.js

---
+++ gallery-app/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <ul className='main-nav'>
+            <li><NavLink to=""></NavLink></li>
+            <li><NavLink to=""></NavLink></li>
+            <li><NavLink to=""></NavLink></li>
+            <li><NavLink to=""></NavLink></li>
+        </ul>
+    </header>
+);
+
+export default Header;
