fd8a0c3e94159e640da6afca3aeb40d5c52081ea
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-29 19:47:51 -0500

    Update Photo.js
    fixing a typo

--- gallery-app/src/Components/Photo.js
+++ gallery-app/src/Components/Photo.js
@@ -5,3 +5,5 @@ const Photo = (props) => (
         <img src={`https://live/staticflickr.com/${props.server}/${props.id}_${props.secret}.jpg`} alt={props.title} />
     </li>
 )
+
+export default Photo
