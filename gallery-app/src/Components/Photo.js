f4b7525f7a21d47cb8bcbb9390950458cc6ffb89
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-11-04 18:10:07 -0500

    Update Photo.js
    Fixed typo

--- gallery-app/src/Components/Photo.js
+++ gallery-app/src/Components/Photo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Photo = (props) => (
     <li>
-        <img src={`https://live/staticflickr.com/${props.server}/${props.id}_${props.secret}.jpg`} alt={props.title} />
+        <img src={`https://live.staticflickr.com/${props.server}/${props.id}_${props.secret}.jpg`} alt={props.title} />
     </li>
 )
 
