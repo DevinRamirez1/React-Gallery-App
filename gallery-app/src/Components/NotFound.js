a76e957fdaad7d9c33167f4193b6f91a704b0101
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-11-04 17:06:07 -0500

    Update NotFound.js
    Added code for NotFound component

--- gallery-app/src/Components/NotFound.js
+++ gallery-app/src/Components/NotFound.js
@@ -1,4 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+const NotFound = () => {
+    return (
+        <ul>
+            <li>
+                <h3>404 Page Not Found</h3>
+                <p>Sorry, your search found no results.</p>
+                <NavLink to="/clouds">Home</NavLink>
+            </li>
+        </ul>
+    );
+}
 
 export default NotFound
