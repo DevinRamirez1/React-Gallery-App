87efe071637a400bee3413525e6818312801d1fb
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-11-09 05:03:14 -0600

    Update PhotoContainer.js
    Removing console.log. Rearranging photos variable

--- gallery-app/src/Components/PhotoContainer.js
+++ gallery-app/src/Components/PhotoContainer.js
@@ -13,8 +13,9 @@ class PhotoContainer extends Component {
 
     render() {
         const results = this.props.data;
-        console.log(results);
-        let photos = results.map( (photo) => {
+        let photos;
+        if (results.length > 0)
+        photos = results.map( (photo) => {
             return <Photo id={photo.id} 
                           server={photo.server} 
                           secret={photo.secret} 
