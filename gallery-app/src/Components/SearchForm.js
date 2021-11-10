e6450d91a0d950e9a34e84ec735893d3cc5cb97b
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-11-10 05:18:57 -0600

    Update SearchForm.js
    Trying to fix bug

--- gallery-app/src/Components/SearchForm.js
+++ gallery-app/src/Components/SearchForm.js
@@ -13,7 +13,8 @@ class SearchForm extends Component {
     handleSubmit = e => {
         e.preventDefault();
         this.props.onSearch(this.query.value);
-        this.props.history.push(`/search/${this.state.searchText}`);
+        let path = `/search/${this.state.searchText}`;
+        this.props.history.push(path);
         e.currentTarget.reset();
     }
 
