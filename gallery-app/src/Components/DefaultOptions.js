ee4cacd127f2786eb3feae32d43ba4ea772ca3e4
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-11-11 05:09:46 -0600

    Update DefaultOptions.js
    Added comments

--- gallery-app/src/Components/DefaultOptions.js
+++ gallery-app/src/Components/DefaultOptions.js
@@ -5,6 +5,7 @@ let rivers = {}
 let mountains = {}
 let dogs = {}
 
+//fetches data for the default choices
 const DefaultOptions = (query) => {
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
@@ -20,6 +21,7 @@ const DefaultOptions = (query) => {
     })
 }
 
+//allows import to import each individual variable
 DefaultOptions('rivers');
 DefaultOptions('mountains');
 DefaultOptions('dogs');
