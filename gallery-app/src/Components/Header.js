782fed641ce5e757ddf3c11493b5235ac723483d
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-11-11 05:05:00 -0600

    Update Header.js
    Added comments

--- gallery-app/src/Components/Header.js
+++ gallery-app/src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+//creates the navlinks for the header component
 const Header = () => (
     <header>
         <ul className='main-nav'>
